95e66813688f68a7a953806dac7c4bb705437009
dit <ditazanelli@gmail.com>
2021-12-26 23:39:34 -0600

    final bug fixes related to localstorage retrieval

--- assets/script.js
+++ assets/script.js
@@ -2,27 +2,37 @@ var day = moment().format("dddd, MMMM Do");
 var currentHr = moment().hour();
 $("#currentDay").text(day);
 
-var workHours = [
-  { time: "9 AM", hour: 09, event: "" },
-  { time: "10 AM", hour: 10, event: "" },
-  { time: "11 AM", hour: 11, event: "" },
-  { time: "12 PM", hour: 12, event: "" },
-  { time: "1 PM", hour: 13, event: "" },
-  { time: "2 PM", hour: 14, event: "" },
-  { time: "3 PM", hour: 15, event: "" },
-  { time: "4 PM", hour: 16, event: "" },
-  { time: "5 PM", hour: 17, event: "" },
-];
+
 
 var saveEvents = function () {
   localStorage.setItem("workHours", JSON.stringify(workHours));
 };
 
+var loadEvents = function () {
+  workHours = JSON.parse(localStorage.getItem("workHours"));
+
+  if (!workHours){
+    workHours = [
+      { time: "9 AM", hour: 09, event: "" },
+      { time: "10 AM", hour: 10, event: "" },
+      { time: "11 AM", hour: 11, event: "" },
+      { time: "12 PM", hour: 12, event: "" },
+      { time: "1 PM", hour: 13, event: "" },
+      { time: "2 PM", hour: 14, event: "" },
+      { time: "3 PM", hour: 15, event: "" },
+      { time: "4 PM", hour: 16, event: "" },
+      { time: "5 PM", hour: 17, event: "" },
+    ];
+  }
+};
+
 var createHour = function (hours, time, event) {
+
+  
   var timeBlock = $("<div id='" + hours + "' class='row timeblock'></div>");
   timeBlock.append("<div class='col p-3 hour'>" + time + "</div>");
   timeBlock.append(
-    "<div class='col-xs col-sm-8 col-lg-10 event'><textarea class='w-100'>" +
+    "<div class='col-xs col-sm-8 col-lg-10 event'><textarea class='w-100 event-text'>" +
       event +
       "</textarea></div>"
   );
@@ -30,6 +40,8 @@ var createHour = function (hours, time, event) {
   $("#schedule").append(timeBlock);
 };
 
+
+loadEvents();
 $.each(workHours, function (i) {
   createHour(workHours[i].hour, workHours[i].time, workHours[i].event);
   if (currentHr === workHours[i].hour) {
@@ -47,36 +59,30 @@ $.each(workHours, function (i) {
   }
 });
 
-$("#schedule").on("click", "textarea", function () {
-  var text = $(this).text().trim();
+$("#currentDay").after($("<button id='clear-btn' class='btn btn-warning'> clear </button>"));
 
-  var textInput = $("<textarea>").val(text);
+$("#schedule").on("click", ".saveBtn", function (event) {
+ event.preventDefault();
 
-  $(this).replaceWith(textInput);
-  textInput.trigger("focus");
-});
-
-$("#schedule").on("click", ".saveBtn", function () {
-  // get the textarea's current value/text
-  var text = $("textarea").val().trim();
-
-  // get the parent id attribute
-  // var hour = $(this)
-  //   .closest("div")
-  //   .attr("id");
-
+loadEvents();  
+var text = $(this).closest(".timeblock").find("textarea").val();
   var index = $(this).closest(".timeblock").index();
+  var hour = workHours[index].hour;
+
+
 
   console.log(text);
   console.log(index);
-  console.log(workHours[index]);
+  // console.log(workHours[index]);
   workHours[index].event = text;
 
   saveEvents();
 });
 
-var loadEvents = function () {
-  workHours = JSON.parse(localStorage.getItem("workHours"));
-};
+$("header").on("click", "#clear-btn", function(){
+  localStorage.clear();
+  location.reload();
 
-loadEvents();
+})
+
+
